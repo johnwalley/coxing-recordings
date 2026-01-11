bd7adea7f5fc81dad76124e56b2ef011501a8418
John Walley <john@walley.org.uk>
2026-01-11 21:12:10 +0000

    New astro site under new folder

---
+++ astro/src/data/recordings.ts
@@ -0,0 +1,174 @@
+export const recordings = [
+  {
+    id: 1,
+    name: "Oxford Brookes University A vs Brown University, USA in the Temple Challenge Cup.",
+    year: "2014",
+    description:
+      "Well there we go - the culmination of a lot of years work, attempts, and the end of the project. After 10 years of rowing, 9 years of racing at Henley Royal Regatta, and 4 years trying for the Temple Challenge Cup (for university 8's), we / I finally won the Temple!",
+    cox: "Rory Copus",
+    imageSrc: "https://www.youtube.com/embed/RDFsB7XwCzY",
+  },
+  {
+    id: 2,
+    name: "Cox Recording - 2011 Rowing World Champs Final LM8+ - Australia",
+    year: "2011",
+    description:
+      "Lightweight men's eight race from the rowing world champs 2011 with coxing call instead of tv commentary.",
+    cox: "David Webster",
+    imageSrc:
+      "https://www.youtube.com/embed/G2ZrDIb6NYI?rel=0&list=PLmDfLlmOxeoFeoSdoLkc-QuwmvBEM1FL9",
+  },
+  {
+    id: 3,
+    name: "Katelin Snyder || University of Washington vs. California Duel 2009 (Coxswain Recording)",
+    year: "2009",
+    description:
+      "Coxswain recording of Katelin Snyder coxing the University of Washington's varsity 8+ in the 2009 duel race against California.",
+    cox: "Katelin Snyder",
+    imageSrc:
+      "https://www.youtube.com/embed/OOjDkWFvPPE?rel=0&list=PLmDfLlmOxeoFeoSdoLkc-QuwmvBEM1FL9",
+  },
+  {
+    id: 4,
+    name: "HRR 2009 QF - Abingdon vs. Belmont Hill",
+    year: "2009",
+    description:
+      "Probably one my most epic races so far - the Quarter final of Henley Royal Regatta 2009, against Belmont Hill School, USA. So epic in fact, my lips went numb during it.",
+    cox: "Rory Copus",
+    imageSrc:
+      "https://www.youtube.com/embed/cM8KfnOYLRQ?rel=0&list=PLmDfLlmOxeoFeoSdoLkc-QuwmvBEM1FL9",
+  },
+  {
+    id: 5,
+    name: "HoRR 2012 - Recording Overlay",
+    year: "2012",
+    description:
+      "Here it is - some video, a few photos, and a recording. Taking a 2nd VIII to the highest placing second boat at the Head of the River Race, and inside the top 10 at 9th position. It was a fantastic row, passing 5 other crews, by an unprecedented crew. I must say, it was the most fun I've had racing the Tideway, and am very proud of the 8 other guys that helped me accomplish it. Enjoy",
+    cox: "Rory Copus",
+    imageSrc:
+      "https://www.youtube.com/embed/qmwPP97fY4Q?rel=0&list=PLmDfLlmOxeoFeoSdoLkc-QuwmvBEM1FL9",
+  },
+  {
+    id: 6,
+    name: "Ladies Plate Final 2017 - Cox Recording",
+    year: "2017",
+    description:
+      "I'm still learning, and don't plan on stopping anytime soon. Improving is half the fun in coxing. I was bloody nervous, some of our plan may not have worked, so I just tried to keep our rhythm long and strong.",
+    cox: "Harry Brightmore",
+    imageSrc: "https://www.youtube.com/embed/qmwPP97fY4Q",
+  },
+  {
+    id: 7,
+    name: "Brookes 8+ @ Metropolitan Regatta 2013",
+    year: "2013",
+    description: "",
+    cox: "Rory Copus",
+    imageSrc: "https://www.youtube.com/embed/sADK5l0La6c",
+  },
+  {
+    id: 8,
+    name: "The 108th Washington v California Dual Cox Tape",
+    year: "2019",
+    description:
+      "Washington vs. California Dual • Montlake Cut, Seattle • April 20, 2019",
+    cox: "Adam Gold",
+    imageSrc: "https://www.youtube.com/embed/y_jsl5JqmWU",
+  },
+  {
+    id: 10,
+    name: "UCSD Varsity 8 WIRA Grand Final 2015 Coxswain Recording",
+    year: "2015",
+    description: "",
+    cox: "Cat Sheffler",
+    imageSrc: "https://www.youtube.com/embed/J0wvtdfwy88",
+  },
+  {
+    id: 11,
+    name: "U23 World Championship Cox Recording - GB Men's 8+ 2021",
+    year: "2021",
+    description:
+      "This is a cox recording of the British Men's U23 Eight in Racice, Czech Republic at 2021 Under 23 World Championships.",
+    cox: "Scott Cockle",
+    imageSrc: "https://www.youtube.com/embed/9RGu9kUolkE",
+  },
+  {
+    id: 12,
+    name: "Brookes C - BUCS Regatta 2019 - Int 8+ Final - Cox Recording",
+    year: "2019",
+    description:
+      "Cox recording of BUCS Regatta 2019 - Intermediate 8 A Final. cox recording. OBUBC",
+    cox: "Max Thompson",
+    imageSrc: "https://www.youtube.com/embed/ChSXgYEWlDQ",
+  },
+  {
+    id: 13,
+    name: "Cal Rowing Varsity 8 Cox Recording - 2018",
+    year: "2018",
+    description: "Stanford Invitational 2018. Redwood Shores.",
+    cox: "John Amorosana",
+    imageSrc: "https://www.youtube.com/embed/l-_IyB_nsVQ",
+  },
+  {
+    id: 14,
+    name: "Henley Royal Regatta Finals Cox Recording - Women's Varsity 8+",
+    year: "2022",
+    description:
+      "Even though we didn’t finish first, I am so beyond proud of each and every one of the girls in this boat.",
+    cox: "Delaney Gardner",
+    imageSrc: "https://www.youtube.com/embed/ZX2ID3SDmt8",
+  },
+  {
+    id: 15,
+    name: "I Mic'd Two Coxes Racing at Henley!",
+    year: "2023",
+    description:
+      "Kieran and Tom, you're legends for letting me mic you two up. Fair play to Washington and Brookes for a stellar race.",
+    cox: "Kieran Joyce and Tom Bryce",
+    imageSrc: "https://www.youtube.com/embed/4BmqmcXCMPU",
+  },
+  {
+    id: 16,
+    name: "I Micd Up the US Men's 8+ Cox at Henley",
+    year: "2022",
+    description:
+      "Such an epic race by the bois and Colette! Pity the rain started pouring all cinematic and then the launch splash made my camera lens go soft.",
+    cox: "Colette Lucas-Conwell",
+    imageSrc: "https://www.youtube.com/embed/BOxYoHi0ugA",
+  },
+  {
+    id: 17,
+    name: "Cox recording - 2022 World Rowing Cup III, Women's Eight A-Final - Kristen Kit, Canada Women's Eight",
+    year: "2022",
+    description:
+      "Listen to Canadian coxswain Kristen Kit's Women's Eight race recording from the 2022 World Rowing Cup III in Lucerne, Switzerland.",
+    cox: "Kristen Kit",
+    imageSrc: "https://www.youtube.com/embed/ZdDz9cORKKg",
+  },
+  {
+    id: 18,
+    name: "Henley Royal Regatta 2024 PE final cox recording",
+    year: "2024",
+    description:
+      "Shiplake College v St. Paul's School in the Princess Elizabeth Challenge Cup.",
+    cox: "Victor Bocquet",
+    imageSrc: "https://www.youtube.com/embed/29CcXBVIEko",
+  },
+  {
+    id: 19,
+    name: "The Boat Race 2022 - Oxford Men’s Cox Recording Highlights",
+    year: "2022",
+    description:
+      "Highlights from the Oxford boat in the 167th Men’s Boat Race, held on the Tideway, 3rd April 2022.",
+    cox: "Jack Tottem",
+    imageSrc: "https://www.youtube.com/embed/I28MS_MLbPg",
+  },
+  {
+    id: 20,
+    name: "Olympic Rowing Men’s Eight Cox Recording",
+    year: "2024",
+    description:
+      "Paris 2024 Men’s Eight Rowing Final.",
+    cox: "Harry Brightmore",
+    imageSrc: "https://www.youtube.com/embed/WJW5m76cpf0",
+  },
+];
